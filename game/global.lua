a2367cf353b54e869ec99287f23e5522cc859975
jdev6 <jd6.dev@github.com>
2015-11-14 22:36:03 +0100

    Should work on windows now

--- game/global.lua
+++ game/global.lua
@@ -5,23 +5,23 @@ function global.fileExists(file)
 end
 
 function global.scandir(directory)
-    local i, t, popen = 0, {}, io.popen --POSIX
-    for filename in popen('ls "'..directory..'"'):lines() do
+  local i, t, popen = 0, {}, io.popen --POSIX
+  for filename in popen('ls "'..directory..'"'):lines() do
+      i = i + 1
+      t[i] = filename
+  end
+  if t[1] == nil then --NT
+    for filename in popen('dir "'..directory..'" /b'):lines() do
         i = i + 1
         t[i] = filename
+        print(t[i])
     end
-    if t[1] == nil then --NT
-      local i, t, popen = 0, {}, io.popen
-      for filename in popen('dir /b "'..directory..'"'):lines() do
-          i = i + 1
-          t[i] = filename
-      end
-    end
-    if t[1] == nil then --Levels folder doesn't exist or it's empty
-      os.execute("mkdir levels")
-      os.execute("mkdir levels/placeholder")
-    end
-    return t
+  end
+  if t[1] == nil then --Levels folder doesn't exist or it's empty
+    os.execute("mkdir levels")
+    os.execute("mkdir levels/placeholder")
+  end
+  return t
 end
 
 return global
