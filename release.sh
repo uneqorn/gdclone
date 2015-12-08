f56db51bfafd6b295e1385373857c25446dd5e33
jdev6 <jd6.dev@github.com>
2015-12-08 17:05:32 +0100

    Created bin folder

--- release.sh
+++ release.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
+echo --EDITOR--
 cd editor
 love-release -W32
-mv releases/.love ../editor.love
-mv releases/love-0.9.2-win32/.exe ../editor.exe
+mv releases/.love ../bin/editor.love
+mv releases/love-0.9.2-win32/.exe ../bin/editor.exe
 rm -r releases
 cd ..
 
+echo --GAME--
 cd game
 love-release -W32
-mv releases/.love ../game.love
-mv releases/love-0.9.2-win32/.exe ../game.exe
+mv releases/.love ../bin/game.love
+mv releases/love-0.9.2-win32/.exe ../bin/game.exe
 rm -r releases
 cd ..
+
